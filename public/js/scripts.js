495e544395188cd49aa4d37e693fe786d272aa4a
philipp-r <philipp-r@users.noreply.github.com>
2016-08-10 18:19:30 +0200

    Fix JS for mobile nav (issue #4)

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,37 +1,3 @@
-
-function update_position(scrollto) {
-	if ($("html").hasClass("lt8")) { return true; }
-	$(".div-affix").each(function() {
-		var elm = $(this),
-			container = elm.parent(),
-			top = $(window).scrollTop(),
-			offset = elm.parent().offset(),
-			classes = "aligned-fixed aligned-bottom";
-		if (elm.height()+24 >= $(window).height()) {
-			if (container.hasClass("aligned-fixed")) {
-				if (scrollto) {
-					container.removeClass(classes).addClass("aligned-bottom");
-					$(window).scrollTop(elm.offset().top);
-				}
-				else {
-					container.removeClass(classes);
-				}
-			}
-
-		}
-		else if (top + 12 <= offset.top) {
-			container.removeClass(classes);
-		}
-		else if (top + 12 + elm.height() >= $("body").height()) {
-			container.removeClass(classes).addClass("aligned-bottom");
-		}
-		else {
-			container.removeClass(classes).addClass("aligned-fixed");
-		}
-	});
-}
-
-
 $(document).ready(function(){
 
 	$(".box-settings .top").click(function() {
@@ -42,9 +8,6 @@ $(document).ready(function(){
 			.closest(".box-settings").find(".inner-form")
 				.slideToggle();
 	});
-	$(".a-help-markdown").click(function() {
-		$(this).closest(".box").find(".div-help-markdown").toggle();
-	});
 
 	$(".alert").click(function() { $(this).slideUp(); });
 
@@ -54,10 +17,5 @@ $(document).ready(function(){
 	$(".a-menu").click(function() {
 		$(".main-right").toggleClass("open");
 	});
-
-// Blocks with fixed position
-	$(window).scroll(function() { update_position(); });
-	$(window).resize(function() { update_position(); });
-	update_position();
 
 });
